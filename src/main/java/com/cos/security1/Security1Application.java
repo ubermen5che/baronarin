727d77e2eccba6c55028694c49610b06274519b2
ubermen5che <ubermen5ch1308@gmail.com>
2021-09-15 22:02:00 +0900

    Remove comments

--- src/main/java/com/cos/security1/Security1Application.java
+++ src/main/java/com/cos/security1/Security1Application.java
@@ -1,13 +1,12 @@
 package com.cos.security1;
 
+import com.cos.security1.config.FileUploadProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
-//@EnableConfigurationProperties(StorageProperties.class)
 @EnableConfigurationProperties(
         {FileUploadProperties.class}
 )
@@ -18,20 +17,6 @@ public class Security1Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(Security1Application.class, args);
 		System.out.print("test1 \n"+ org.springframework.core.SpringVersion.getVersion());
-		
-		
-		/*
-		 Mail mail = new Mail();
-	        mail.setMailFrom("from@gmail.com");
-	        mail.setMailTo("terizeto1009gmail.com");
-	        mail.setMailSubject("This is Email test.");
-	        mail.setMailContent("Learn how to send email using Spring.");
-	 
-	        AbstractApplicationContext context = new AnnotationConfigApplicationContext(MailConfig.class);
-	        MailService mailService = (MailService) context.getBean("mailService");
-	        mailService.sendEmail(mail);
-	        context.close();
-		 */
 
 	}
 	
@@ -39,15 +24,4 @@ public class Security1Application extends SpringBootServletInitializer{
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
 	{ return builder.sources(Security1Application.class);
 	}
-	/*
-	@Bean
-	CommandLineRunner init(StorageService storageService)
-	{
-		return (args) -> {
-			storageService.deleteAll();
-			storageService.init();
-		};
-	}
-	*/
-
 }
