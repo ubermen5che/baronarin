6f482501ea463869a743eb6ef5cbd2f4d3b0ffa6
ubermen5che <ubermen5ch1308@gmail.com>
2021-10-13 16:14:45 +0900

    username -> email 필드변수명 변경 및 전화번호 저장을 위한 멤버변수 추가

--- src/main/java/com/cos/security1/domain/User.java
+++ src/main/java/com/cos/security1/domain/User.java
@@ -23,13 +23,16 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)//기본 varchar(255)
 	private int id;
-	private String realname;
-	private String username;//username이 이메일
+	private String realName;
+	private String email;//username이 이메일
 	private String password;
+	private String phoneNumber;
+	private String authKey;
+	private String authStatus;
 	//private String email;
 	private String role;//ROLE_USER, ROLE_ADMIN
 	//private Timestamp loginDate;//로그인 할 때 마다 갱신
-	private String signname;//유저 사인 이미지이름
+	private String signName;//유저 사인 이미지이름
 	
 	@Column(columnDefinition = "TEXT",length=1000)//text 길이는 1000
 	private String publicKey;
