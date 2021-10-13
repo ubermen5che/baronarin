31777b1ea452a233a30af2aefe3fef635007da47
ubermen5che <ubermen5ch1308@gmail.com>
2021-10-13 16:23:39 +0900

    Mail 정보 일괄 수정을 위한 클래스 추가 및 Mail전송 관련 Util추가

---
+++ src/main/java/com/cos/security1/common/MailInfo.java
@@ -0,0 +1,8 @@
+package com.cos.security1.common;
+
+public final class MailInfo {
+
+    public static final String GMAIL_ID = "";
+    public static final String GMAIL_PW = "";
+    public static final String ADMIN_ID = "";
+}
