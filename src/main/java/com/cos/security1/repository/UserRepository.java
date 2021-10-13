c68daa2e6da43ff38a60cd5dba8b3779b70ec5d3
ubermen5che <ubermen5ch1308@gmail.com>
2021-10-13 16:21:23 +0900

    User 관련 DB처리 코드 추가

--- src/main/java/com/cos/security1/repository/UserRepository.java
+++ src/main/java/com/cos/security1/repository/UserRepository.java
@@ -3,17 +3,18 @@ package com.cos.security1.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cos.security1.domain.User;
-
-
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 //CRUD함수를 JpaRepository가 들고 있다
 //@Repository 어노테이션 없어도 IOC가 됨, 상속했기 떄문
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 
 	//findBy 규칙-> Username문법
 	//select * from user where username = 1? 파라미터에 들어온 내용을 넣어줌
-	public User findByUsername(String username);
-	
+	public User findByEmail(String email);
+	public User findByAuthKey(String authKey);
 	//public User findByEmail(String email);
 }
